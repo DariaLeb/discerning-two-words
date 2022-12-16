a113eb09145e61776c5235ed103396430907802a
daria.leb@post.cz <daria.leb@post.cz>
2022-12-16 12:14:28 +0100

    [plots] refactor

--- plots.py
+++ plots.py
@@ -1,31 +1,54 @@
 import math
+import pickle
+from pathlib import Path
 
 import matplotlib.pyplot as plt
 
 
+"""
+This module is used to plot simple line charts with evolution of some variable for different ns.
+It could be maximum m, number of examples, min/max sum etc.
+"""
+
 if __name__ == '__main__':
 
-    ns = range(6, 32, 2)
-    ms = [4, 5, 5, 6, 7, 7, 7, 8, 8, 9, 9, 9, 10]
+    with Path('data/results_unique_62.pickle').open('rb') as f:
+        result = pickle.load(f)
 
-    min_s = [4, 4, 4, 6, 6, 6, 4, 8, 8, 12, 12, 8, 16]
-    max_s = [4, 6, 8, 8, 8, 12, 12, 8, 16, 14, 20, 20, 18]
+    ns = list(result)
+    ms = [val[0] for val in result.values()]
+
+    size = [int(math.log2(len(val[1]))) for val in result.values()]
+
+    min_s = [min(len(ex[0]) for ex in val[1]) for val in result.values()]
+    max_s = [max(len(ex[0]) for ex in val[1]) for val in result.values()]
+
+    number_of_s = [len(val[1]) for val in result.values()]
 
     log = [math.log(n) for n in ns]
-    half = [n // 2 for n in ns]
+    sqrt = [math.sqrt(n) for n in ns]
+    half = [n / 2 for n in ns]
+    square = [n / 4 for n in ns]
+    fives = [n / 5 for n in ns]
+    sixs = [n / 6 for n in ns]
 
     plt.figure()
-    plt.plot(ns, ms, label='m')
-    plt.plot(ns, min_s, label='min |s|')
-    plt.plot(ns, max_s, label='max |s|')
+    # plt.plot(ns, ms, '.-', label='m')
+    plt.plot(ns, size, '.-', label='log number of unique examples')
+    # plt.plot(ns, min_s, label='min |s|')
+    # plt.plot(ns, max_s, label='max |s|')
     # plt.plot(ns, log, label='log')
-    plt.plot(ns, half, label='n/2')
+    # plt.plot(ns, sqrt, label='sqrt')
+    # plt.plot(ns, square, label='n/4')
+    # plt.plot(ns, fives, label='n/5')
+    # plt.plot(ns, sixs, label='n/6')
+    # plt.plot(ns, half, label='n/2')
+    # plt.plot(ns, number_of_s, label='number of S')
 
     plt.xlabel('n')
 
     plt.legend()
     plt.grid()
 
-    plt.savefig('images/results_sizes.jpg')
+    # plt.savefig('images/number_unique.pdf')
     plt.show()
-
