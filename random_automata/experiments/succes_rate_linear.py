18109075a39c53426a9267b08c045c6a3c82b096
daria.leb@post.cz <daria.leb@post.cz>
2023-04-02 10:37:52 +0200

    [success_rate_linear] optimize, store history

--- random_automata/experiments/succes_rate_linear.py
+++ random_automata/experiments/succes_rate_linear.py
@@ -8,7 +8,7 @@ from time import time
 import numpy as np
 import matplotlib.pyplot as plt
 
-from random_automata.automaton import shifted_permutation_automaton
+from random_automata.automaton import shifted_permutation_automaton, permutation_automaton, random_automaton
 
 """
 In this module we are looking for minimal successful automaton size for a given percentage of changes in the word. 
@@ -45,10 +45,14 @@ def run_experiment(automaton, word_len, n_words, n_tries, n_changes):
     first_n = None
     n = start
 
+    history = []
+    ns = []
+
     while True:
 
+        ns.append(n)
+
         rate = 0
-        succ = True
         for i in range(n_words):
 
             x, y = random_pair(word_len, n_changes)
@@ -63,13 +67,14 @@ def run_experiment(automaton, word_len, n_words, n_tries, n_changes):
                         rate += 1
                         break
                 else:
-                    succ = False
                     break
 
-            if not succ:
+            # early stopping condition - if automaton already failed, this size is not successful
+            if rate != n_tries * (i + 1):
                 break
 
-        # print(f'm = {word_len}, {n = }: success rate = {rate / (n_tries * n_words)}' )
+        history.append(rate)
+
         if rate == n_tries * n_words:
             if success == 0:
                 first_n = n
@@ -77,44 +82,40 @@ def run_experiment(automaton, word_len, n_words, n_tries, n_changes):
         else:
             success = 0
 
-        if success >= 5:
+        if success >= 10:
             break
 
-        n += 1
+        n += 5
 
-    return first_n
+    bool_history = map(lambda x: int(x == n_tries * n_words), history)
+    bool_history = ', '.join(map(str, bool_history))
+    print(f'Finish m={word_len}, n={first_n}\nstart={start}\nsuccess rates: {bool_history}\nhistory: {history}')
 
-
-def plot_results(results, ns, n_words, n_tries, n_changes, style):
-
-    for n_change, res in zip(n_changes, results):
-        avg = res.sum(axis=1) / (n_tries * n_words)
-        n_log = [math.log(n) for n in ns]
-
-        plt.plot(n_log, avg, linestyle=style, marker='.', label=f'{n_change} changes')
+    return first_n, history[:-9], ns[:-9]
 
 
 def main():
 
-    n_words = 100
-    n_tries = 1000
+    n_words = 1000
+    n_tries = 100
 
-    ms = list(range(50, 4000, 50))
+    ms = range(50, 1050, 50)
 
-    for n_changes in [0.2,  0.5, 0.7, 0.9, 1.0]:
+    for n_changes in [0.5, 1]:
         task = partial(
             run_experiment,
-            shifted_permutation_automaton,
+            random_automaton,
             n_words=n_words, n_tries=n_tries, n_changes=n_changes
         )
 
         with Pool() as pool:
             res = pool.map(task, ms)
 
+        res, history, ns = zip(*res)
         rand_key = np.random.randint(10000)
-        path = Path(f'../data/linear_{n_changes}_{rand_key}.pickle')
+        path = Path(f'../data/linear_usual_{n_changes}_{rand_key}.pickle')
         with open(path, 'wb') as f:
-            pickle.dump({'ms': ms, 'res': res}, f)
+            pickle.dump({'ms': ms, 'res': res, 'history': history, 'ns': ns}, f)
 
         plt.figure()
         plt.plot(ms, res)
@@ -122,10 +123,27 @@ def main():
         plt.ylabel('n = minimal successful size')
         plt.xlabel('m = word length')
 
-        plt.title(f'Shifted permutation automaton, {int(n_changes * 100)}% of changes in the word')
+        plt.title(f'Random automaton, {int(n_changes * 100)}% of changes in the word')
 
         plt.tight_layout()
-        plt.savefig(f'../images/linear_size_changes_{n_changes}_{rand_key}.pdf')
+        plt.savefig(f'../images/linear_random_size_changes_{n_changes}_{rand_key}.pdf')
+
+        for i in list(range(0, len(ms), 4)) + [len(ms) - 1]:
+            h = history[i]
+            h = [j * 100 / (n_tries * n_words) for j in h]
+            n = ns[i]
+            m = ms[i]
+
+            plt.figure()
+            plt.plot(n, h)
+
+            plt.ylabel('Percentage before first fail [%]')
+            plt.xlabel('n = automaton size')
+
+            plt.title(f'Random automaton, {int(n_changes * 100)}% of changes in the word, m = {m}')
+
+            plt.tight_layout()
+            plt.savefig(f'../images/linear_random_size_history_{n_changes}_{m}_{rand_key}.pdf')
 
 
 if __name__ == '__main__':
