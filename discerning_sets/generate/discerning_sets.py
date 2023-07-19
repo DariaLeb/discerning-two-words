77d8787e5a0927f6ce409c282edc739b5f38f8f9
daria.leb@post.cz <daria.leb@post.cz>
2023-07-19 19:09:47 +0200

    [discerning_sets] refactor

--- discerning_sets/generate/discerning_sets.py
+++ discerning_sets/generate/discerning_sets.py
@@ -3,17 +3,17 @@ from itertools import chain, combinations
 from pathlib import Path
 
 """
-This module generates example subset for even ns in the given interval. 
+This module generates critical sets for ns in the given interval. 
 For this, it tests all subset of [n] with even number of elements and founds ones with maximum `m`.
 Maximum computed number was n=34 (runs over a day on my laptop).
 
-For each n, it stores generated examples in `data/results_{n}.pickle` for reproducibility.
-In pickle file, there is a dictionary, where for each n list of examples in the following format is stored:
+For each n, it stores generated sets in `data/results_{n}.pickle` for reproducibility.
+In pickle file, there is a dictionary, where for each n list of critical sets in the following format is stored:
 n -> (max_m, examples, c), 
 where `examples` is the list of examples and `c` is a number of processed subsets. 
-Examples are stored in the following format:
+Critical sets are stored in the following format:
 [(s, m, i, counts)], 
-where `s` is a subset, `m` is m for this subset, `i` is the residue where violation for given `m` occurs and 
+where `s` is a set, `m` is m for this set, `i` is the residue where violation for given `m` occurs and 
 `counts` is a list with number of elements in each residue class.
 """
 
@@ -45,7 +45,7 @@ def analyze_n(n):
 
     max_m = 0
     c = 0
-    examples = []
+    sets = []
 
     for s in even_subsets(n):
 
@@ -57,34 +57,34 @@ def analyze_n(n):
             if i is not None:
                 # print(f'{s=}: {m=}, {i=}, {counts=}')
                 if m == max_m:
-                    examples.append((s, m, i, counts))
+                    sets.append((s, m, i, counts))
                 elif m > max_m:
                     max_m = m
-                    examples = [(s, m, i, counts)]
+                    sets = [(s, m, i, counts)]
                 break
 
         else:
             if max_m == n:
-                examples.append((s, None, None, None))
+                sets.append((s, None, None, None))
             elif n > max_m:
                 max_m = n
-                examples = [(s, None, None, None)]
+                sets = [(s, None, None, None)]
 
-    return max_m, examples, c
+    return max_m, sets, c
 
 
 def main():
 
     result = {}
 
-    for n in range(6, 40, 2):
-        max_m, examples, c = analyze_n(n)
-        result[n] = max_m, examples, c
+    for n in range(6, 40):
+        max_m, sets, c = analyze_n(n)
+        result[n] = max_m, sets, c
 
         print('---' * 3)
         print(f'for {n = } {c} subsets were processed')
         print(f'for {n = } max m is {max_m}')
-        for s, m, i, counts in examples:
+        for s, m, i, counts in sets:
             print(f'{s = }, {m = }, {i = }, {counts = }')
         print()
 
